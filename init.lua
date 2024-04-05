79a8e8e185295e1a30b238d0c6213aacf2852da6
Chai <kckavuri@gmail.com>
2024-04-05 07:14:10 +0100

    added relative line numbers

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
+vim.wo.relativenumber = true
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
 
